05860a7feec2aed45b94497639138e9a0f57f723
sj <1294606014@qq.com>
2021-03-16 23:36:45 +0800

    1.2版本 增加了注册功能

--- src/test/java/com/nowcode/community/MailTest.java
+++ src/test/java/com/nowcode/community/MailTest.java
@@ -1,5 +1,8 @@
 package com.nowcode.community;
 
+import com.nowcode.community.dao.UserMapper;
+import com.nowcode.community.entity.User;
+import com.nowcode.community.service.UserService;
 import com.nowcode.community.util.MailClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +18,9 @@ import org.thymeleaf.context.Context;
 @ContextConfiguration(classes = CommunityApplication.class)
 public class MailTest {
     @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
     private MailClient mailClient;
 
     @Autowired
@@ -27,9 +33,18 @@ public class MailTest {
     public void testHtmlMail(){
         Context context =new Context();
         context.setVariable("username","sunday");
-     String content = templateEngine.process("/mail/demo",context);
+        String content = templateEngine.process("/mail/demo",context);
         System.out.println(content);
         mailClient.sendMail("2645044053@qq.com","html",content);
 
+    }
+    @Test
+    public void findByUsername(){
+        User user= userMapper.selectByName("niuke");
+       if(user!=null){
+            System.out.println(user);
+        }else{
+           System.out.println("null");
+        }
     }
 }
