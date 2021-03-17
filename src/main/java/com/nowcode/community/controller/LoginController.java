089992db985c9c89cf2e61723c31ff5129d4822a
sj <1294606014@qq.com>
2021-03-17 18:36:01 +0800

    1.4版本 增加了刷新验证码功能

--- src/main/java/com/nowcode/community/controller/LoginController.java
+++ src/main/java/com/nowcode/community/controller/LoginController.java
@@ -1,9 +1,13 @@
 package com.nowcode.community.controller;
 
+import com.google.code.kaptcha.Producer;
+import com.nowcode.community.config.KaptchaConfig;
 import com.nowcode.community.entity.User;
 import com.nowcode.community.service.UserService;
 import com.nowcode.community.util.CommunityConstant;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,14 +15,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
+
 
 @Controller
 public class LoginController implements CommunityConstant {
 
+    private  static  final Logger logger=LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private Producer kaptchaProducer;
+
 
     @RequestMapping(path = "/register",method = RequestMethod.GET)
     public String getRegisterPage(){
@@ -63,5 +80,26 @@ public class LoginController implements CommunityConstant {
 
     }
 
+    /**
+     * 生成验证码图片
+     */
+    @RequestMapping(path = "/kaptcha",method = RequestMethod.GET)
+    public void getKaptcha(HttpServletResponse response, HttpSession session){
+        //生成验证码
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+
+        //将验证码存入session
+        session.setAttribute("kaptcha",text);
+
+        //将图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            OutputStream os = response.getOutputStream();
+            ImageIO.write(image,"png",os);
+        } catch (IOException e) {
+            logger.error("响应验证码失败："+e.getMessage());
+        }
+    }
 
 }
